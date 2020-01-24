806fae9e0e73fe616676d59744663b2cb4ec3ec6
shirzady1934 <shirzady.1934@gmail.com>
2020-01-24 22:09:12 +0330

    removed publish in models and change type of created date and registered it to admins

--- blog/models.py
+++ blog/models.py
@@ -3,18 +3,21 @@ from django.utils import timezone
 from django.conf import settings
 from django.contrib.auth.models import User
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
     title = models.CharField(max_length = 250)
     text = models.TextField()
-    created_date = models.DateField(default = timezone.now) 
-    published_date = models.DateTimeField(blank=True, null=True)
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
+    created_date = models.DateTimeField(default=timezone.now) 
     def __str__(self):
         return self.title
 class Token(models.Model):
-    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     token = models.CharField(max_length = 20)
     def __str__(self):
         return self.token
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=25)
+    age = models.IntegerField()
+    location = models.CharField(max_length=20)
+    def __str__(self):
+        return self.user.username
