b129603b1d95ef3c0954259bb2311f21dcf2694a
shirzady1934 <shirzady.1934@gmail.com>
2020-01-24 22:09:39 +0330

    create new url for login home and other!

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-        path('<int:post_id>/', views.post_list, name = 'post_list'),
+        path('<int:post_id>/', views.post_show, name = 'post_show'),
         path('submit_post', views.submit_post, name = 'submit_post'),
         path('delete_post', views.delete_post, name = 'delete_post'),
         path('posts/<str:p_user>/<int:p_id>/', views.user_post, name = 'posts'),
         path('posts/<str:username>/', views.user, name = 'posts_user'),
         path('publish', views.publish, name = 'publish'),
-        path('', views.new_post, name = 'new_post'),        
+        path('', views.new_post, name = 'new_post'),     
+        path('login', views.login, name='login'),
+        path('logout', views.logout, name='logout'),
+        path('home', views.home, name='home'),
 ]
 
