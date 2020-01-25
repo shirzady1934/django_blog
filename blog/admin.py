fe62d15edd95c78faf7028f20b06141521d64fb0
shirzady1934 <shirzady.1934@gmail.com>
2020-01-25 18:38:01 +0330

    add comment to admin pangel!

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Post, Token, UserProfile
+from .models import Post, Token, UserProfile, Comment
 admin.site.register(Post)
 admin.site.register(Token)
 admin.site.register(UserProfile)
+admin.site.register(Comment)
