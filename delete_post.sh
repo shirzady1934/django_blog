6105234cc44c15bff635b12b6266a4937f3a6797
shirzady1934 <shirzady.1934@gmail.com>
2020-01-25 10:56:08 +0330

    delete the shell api cause of add the csrf and login session!

--- delete_post.sh
+++
@@ -1,1 +0,0 @@
-curl -d 'token=chicha12&post_id=1' 127.0.0.1:8000/blog/delete_post
--- submit_post.sh
+++
@@ -1,1 +0,0 @@
-curl -d 'token=blowdoor12&title=asghar say hello&text= i wanna to say hello to all user' localhost:8000/blog/submit_post
