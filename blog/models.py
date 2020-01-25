5ccd2616e90e9b3ff96c030a7116be79338ba531
shirzady1934 <shirzady.1934@gmail.com>
2020-01-25 18:38:42 +0330

    added comment model to this blog!

--- blog/models.py
+++ blog/models.py
@@ -21,3 +21,10 @@ class UserProfile(models.Model):
     location = models.CharField(max_length=20)
     def __str__(self):
         return self.user.username
+class Comment(models.Model):
+    text = models.TextField(max_length=200)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+    def __str__(self):
+        return ("%s : %s" % (self.author.username, self.text))
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
-from .models import Post, Token, UserProfile
+from .models import Post, Token, UserProfile, Comment
 from django.contrib.auth.models import User
 from django.contrib.auth import logout
 from django.contrib.auth.hashers import check_password, make_password
@@ -45,24 +45,27 @@ def publish(request):
         return Http404("error")
 def user_show(request, username):
     try:
-        User.objects.get(pk=request.session['member_id'])
+        me = User.objects.get(pk=request.session['member_id'])
         log_status = True
     except:
         log_status = False
     this_user = User.objects.get(username=username)
     post_list = [ post for post in Post.objects.filter(author = this_user).order_by('-created_date')]
-    context = { 'post_list' : post_list, 'log_status' : log_status}
+    this_profile = get_object_or_404(UserProfile, user=this_user)
+    is_owner = True if me != None and me.username == this_user.username else False
+    context = { 'post_list' : post_list, 'log_status' : log_status, 'profile' : this_profile, 'is_owner' : is_owner}
     return render(request, 'blog/user_profile.html', context)
 
 def new_post(request):
-	try:
-		User.objects.get(pk=request.session['member_id'])
-		log_status = True
-	except:
-		log_status = False
-	news = Post.objects.order_by('-created_date')
-	context = { 'post_list' : news, 'log_status' : log_status}
-	return render(request, 'blog/news.html', context)
+    try:
+        me = User.objects.get(pk=request.session['member_id']).username
+        log_status = True
+    except:
+        me = None
+        log_status = False
+    news = Post.objects.order_by('-created_date')
+    context = { 'post_list' : news, 'log_status' : log_status, 'username' : me}
+    return render(request, 'blog/news.html', context)
 def user_post(request, username, id):
     try:
         me = User.objects.get(pk=request.session['member_id'])
@@ -74,14 +77,28 @@ def user_post(request, username, id):
     this_user = get_object_or_404(User, username=username)
     this_post = get_object_or_404(Post, id=id)
     is_owner = True if me != None and me.username == this_post.author.username else False
-
+    comments = Comment.objects.filter(post=this_post)
+    comments = comments if len(comments) != 0 else None
     if this_post.author.username == this_user.username:
-        context = { 'post' : this_post, 'log_status' : log_status, 'is_owner' : is_owner}
+        context = { 'post' : this_post, 'comments': comments,'log_status' : log_status, 'is_owner' : is_owner}
         return render(request, 'blog/post_show.html', context)
     else:
         return Http404("Page not found")
-def default(request):
-    return HttpResponse("Hey wellcome to blog section")
+def submit_comment(request):
+    if request.method == 'POST':
+        if 'member_id' in request.session:
+            if 'text' in request.POST and 'post_id' in  request.POST:
+                this_post = get_object_or_404(Post, pk=request.POST['post_id'])
+                text = request.POST['text']
+                this_user = get_object_or_404(User, id=request.session['member_id'])
+                Comment.objects.create(text=text, post=this_post, author=this_user)
+                return HttpResponseRedirect('/blog/profile/%s/%d' % (this_post.author, this_post.id))
+            else:
+                return HttpResponse("please fill all forms!")
+        else:
+            return HttpResponse("please first login!")
+    else:
+        return HttpResponse("hello :)")
 def check_login(user, passwd):
 	try:
 		this_user = User.objects.get(username=user)
@@ -99,7 +116,7 @@ def login(request):
             passwd = request.POST['password']
             if check_login(user, passwd):
                 this_user = User.objects.get(username=user)
-                request.session ['member_id'] = this_user.id
+                request.session['member_id'] = this_user.id
                 return HttpResponseRedirect('/blog', "you loged in succesfully")
             else:
                 return HttpResponse("username or pass dosent match")
@@ -150,4 +167,3 @@ def sign_up(request):
             HttpResponse("please fill up all forms")
     else:
         return render(request, 'blog/signup.html')
-
