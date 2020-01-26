03ad5e6fb5c926b184ace741b0c6a916d12bece9
shirzady1934 <shirzady.1934@gmail.com>
2020-01-26 17:12:12 +0330

    added notification to admin panel!

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post, Token, UserProfile, Comment
+from .models import Post, Token, UserProfile, Comment, Notification
 admin.site.register(Post)
 admin.site.register(Token)
 admin.site.register(UserProfile)
 admin.site.register(Comment)
+admin.site.register(Notification)
