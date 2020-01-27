df1e0bd04d1fee8d55610915e68c5841472df247
shirzady1934 <shirzady.1934@gmail.com>
2020-01-27 17:11:56 +0330

    add session login manage to admin section!

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Post, Token, UserProfile, Comment, Notification
+from django.contrib.sessions.models import Session
 admin.site.register(Post)
 admin.site.register(Token)
 admin.site.register(UserProfile)
 admin.site.register(Comment)
 admin.site.register(Notification)
+admin.site.register(Session)
