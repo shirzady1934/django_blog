8ac48b9de42e4ec019756505c72fa3667c8f6166
shirzady1934 <shirzady.1934@gmail.com>
2020-02-04 19:36:24 +0330

    added the /blog/api section restfull api!

--- mysite/urls.py
+++ mysite/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
+    path('blog/api/', include('blog.api.urls')),
     path('', views.blog),
 ]
