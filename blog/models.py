cbdb5ddafd2f3df741876f91e543c28a889b92a8
shirzady1934 <shirzady.1934@gmail.com>
2020-02-04 15:43:23 +0330

    added rest_framework and it's token to installed app and add token when user created to the user by django.signal post_save and dispatch.receiver

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.utils import timezone
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete = models.CASCADE)
     title = models.CharField(max_length = 250)
@@ -35,3 +39,8 @@ class Notification(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     def __str__(self):
         return ('%s: %s' % (self.user.username, self.type))
+
+@receiver(post_save, sender=User)
+def create_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
